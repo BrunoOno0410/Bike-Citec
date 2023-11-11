ff25b54023fa046f84fb1ca575befc45aee083a3
BrunoOno0410 <brunogiordano.ono@gmail.com>
2023-11-11 18:09:07 -0300

    Files removal

--- App.js
+++ App.js
@@ -1,26 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { LoginForm } from "./screens/login";
 import { AccountCreateForm, CreateUser } from "./screens/createUser";
 import { HomeScreen } from "./screens/home";
-import {
-  AccountPasswordChangeForm,
-  ForgotPassword,
-} from "./screens/forgotPassword";
+import { AccountPasswordChangeForm } from "./screens/forgotPassword";
 
 const Stack = createNativeStackNavigator();
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ffffff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default function App() {
   return (
